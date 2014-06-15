9d5cbc8547d05fc151364c1064b16526c9e8ca8a
Scott Grogan <scott@ninjascript.com>
2014-06-14 21:15:58 -0700

    Exposing configurable redis client service

--- services/redis.js
+++ services/redis.js
@@ -1,11 +1,39 @@
+var _ = require('lodash');
 var redis = require('redis');
-var client = redis.createClient();
+var defaults = {
+    port: 6379,
+    host: '127.0.0.1',
+    options: {}
+};
 
-client.DB_INDEX_APP = '0';
-client.DB_INDEX_TEST = '9';
+var RedisService = function() {};
 
-client.on('error', function(msg) {
-    console.error(msg);
-});
+RedisService.prototype = Object.create(Object.prototype);
+RedisService.prototype.constructor = RedisService;
 
-module.exports = client;
+RedisService.prototype.configure = function(port, host, options) {
+
+    var port = port || defaults.port;
+    var host = host || defaults.host;
+    var options = _.defaults({}, options, defaults.options);
+    var client = null;
+
+    if (this.client) {
+        this.client.quit();
+    }
+
+    client = redis.createClient(port, host, options);
+    client.on('error', console.error);
+
+    Object.defineProperties(this, {
+        port:    { value: port,    writable: false },
+        host:    { value: host,    writable: false },
+        options: { value: options, writable: false },
+        client:  { value: client,  writable: false }
+    });
+
+    return this;
+};
+
+module.exports = new RedisService();
+
--- tests/services/redis.js
+++ tests/services/redis.js
@@ -1,41 +1,21 @@
 var mocha = require('mocha');
 var should = require('chai').should();
-var redis = require('../../services/redis');
+var redis = require('../../services/redis').configure();
 
 describe('redis service', function() {
-
-    beforeEach(function(done) {
-
-        // Make sure we're on the TEST db and flush it
-        redis.select(redis.DB_INDEX_TEST);
-        redis.flushdb(done);
-    });
 
     it ('can set and retrieve string values', function(done) {
 
         var key = 'foo';
         var value = 'bar';
+        var client = redis.client;
 
-        redis.set(key, value, function(err, result) {
+        client.set(key, value, function(err, result) {
             'OK'.should.equal(result);
-            redis.get(key, function(err, result) {
+            client.get(key, function(err, result) {
                 value.should.equal(result);
                 done();
             });
-        });
-    });
-
-    it ('can set and retrieve object values', function(done) {
-
-        var key = 'foo';
-        var value = { bar: 'baz' };
-
-        redis.hmset(key, value, function(err, result) {
-            'OK'.should.equal(result);
-            redis.hgetall(key, function(err, result) {
-                value.should.deep.equal(result);
-                done();
-            }); 
         });
     });
 });
